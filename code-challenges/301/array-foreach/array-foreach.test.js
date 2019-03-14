8d07df18f0165bef65534d845edf48ee648c25ea
Gregory Dukes <kcils360@gmail.com>
2019-03-13 17:30:10 -0700

    completed challange 2-4

--- code-challenges/301/array-foreach/array-foreach.test.js
+++ code-challenges/301/array-foreach/array-foreach.test.js
@@ -10,12 +10,13 @@ Then, write a function named speaker that takes in a string and a callback funct
 
 const greeting = (word) => {
   // Solution code here...
-  return(word.toUpperCase());
+  return word.toUpperCase();
 }
 
 const speaker = (message, callback) => {
   // Solution code here...
-  callback(message);
+  return callback(message);
+
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -36,10 +37,15 @@ Return the modified array.
 
 const addValues = (arr, value) => {
   // Solution code here...
+  arr.push(value);
 }
 
 const addNumbers = (num, arr, times, callback) => {
   // Solution code here...
+  for(var i=0; i < times; i++){
+      callback(arr, num);
+    }
+    return arr;
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -56,11 +62,19 @@ Return the modified array.
 
 const removeOne = (num, arr) => {
   // Solution code here...
+  if(num % 3 === 2){
+    arr.pop();
+  }
 }
 
 const removeElements = (arr, callback) => {
   // Solution code here...
+  for(var i=0; i<arr.length; i++){
+      callback(arr[i], arr)
+  }
+  return arr;
 }
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
@@ -70,6 +84,10 @@ Write a function named removeWithForEach that produces the same output as challe
 
 const removeWithForEach = (arr, callback) => {
   // Solution code here...
+  arr.forEach( (num, idx) => {
+    callback(num, arr) 
+  });
+  return arr;
 }
 
 /* ------------------------------------------------------------------------------------------------
