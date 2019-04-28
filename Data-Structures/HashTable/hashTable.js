281bbfe89ebabedf123a4f1bf9bc351514b0b1a0
kcils360@live.com <kcils360@live.com>
2019-04-27 21:17:16 -0700

    completed implimentation and README

---
+++ Data-Structures/HashTable/hashTable.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const LinkedList = require('./linkedList');
+
+class hashTable{
+  constructor(size){
+    this.size = size;
+    this.arr = new Array(size);
+  }
+  hash(key){
+    //does neat math to create a hash value of the key into a position in the array
+    return key.split('').reduce((p,n) => {
+      return p + n.charCodeAt(0)
+    }, 0) * 599 % this.size;
+  }
+  add(key, value){
+    //inserts the value at the position in the array based on the hash
+    let hash = this.hash(key);
+    if(!this.arr[hash]){
+      this.arr[hash] = new LinkedList();
+    }
+    let htValue = {[key]: value};
+    this.arr[hash].append(htValue);
+  }
+  get(key){
+    //returns the value of of a matching key
+    if(!contains(key)){
+      return null;
+    }
+    return arr[find].values();
+  }
+  contains(key){
+    //returns a boolean, is key in the hash table
+    let find = hash(key);
+    if (!this.arr[find]) {
+      return false;
+    }
+    return true;
+  }
+
+}
